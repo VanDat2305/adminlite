6aee6310be4ed864d33fefac9f2254c81842b57a
datmvph17185@fpt.edu.vn <datmvph17185@fpt.edu.vn>
2022-02-27 22:19:33 +0700

    'dat'

---
+++ sidebar.blade.php
@@ -0,0 +1,32 @@
+<li class="nav-item">
+            <a href="{{ BASE_URL }}dashboard" class="nav-link ">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ BASE_URL }}mon-hoc" class="nav-link ">
+              <i class="nav-icon fas fa-book"></i>
+              <p>
+                Môn học
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ BASE_URL }}sinh-vien" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Sinh viên
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ BASE_URL }}logout" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Đăng xuất
+              </p>
+            </a>
+          </li>
